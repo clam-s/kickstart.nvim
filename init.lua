2f24568d3781e0141bb4d66ea9185a42853f0d23
Aydin Zularin <aydinzulazrin@gmail.com>
2025-11-02 14:28:16 +0800

    removed souble s binding

--- init.lua
+++ init.lua
@@ -184,10 +184,6 @@ require('lazy').setup({
     end,
   },
   {
-    'tpope/vim-surround',
-    lazy = true, -- Load the plugin lazily (optional)
-  },
-  {
     'nvim-tree/nvim-tree.lua',
     dependencies = { 'nvim-tree/nvim-web-devicons' }, -- optional, for file icons
     config = function()
